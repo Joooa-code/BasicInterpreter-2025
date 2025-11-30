d79f17845264fa3152de31b7fa60b90f39cd4b85
joooa <wangxinbei@sjtu.edu.cn>
2025-11-30 22:44:42 +0800

    fix program

--- include/Statement.hpp
+++ include/Statement.hpp
@@ -23,3 +23,74 @@ class Statement {
 
 // TODO: Other statement types derived from Statement, e.g., GOTOStatement,
 // LetStatement, etc.
+
+// LET
+class LetStatement : public Statement {
+private:
+    std::string variable_;
+    Expression* expression_;
+
+public:
+    LetStatement(const std::string& source, const std::string& var, Expression* expr);
+    ~LetStatement();
+    void execute(VarState& state, Program& program) const override;
+};
+
+// PRINT
+class PrintStatement : public Statement {
+private:
+    Expression* expression_;
+
+public:
+    PrintStatement(const std::string& source, Expression* expr);
+    ~PrintStatement();
+    void execute(VarState& state, Program& program) const override;
+};
+
+// INPUT
+class InputStatement : public Statement {
+private:
+    std::string variable_;
+
+public:
+    InputStatement(const std::string& source, const std::string& var);
+    void execute(VarState& state, Program& program) const override;
+};
+
+// GOTO
+class GotoStatement : public Statement {
+private:
+    int targetLine_;
+
+public:
+    GotoStatement(const std::string& source, int line);
+    void execute(VarState& state, Program& program) const override;
+};
+
+// IF
+class IfStatement : public Statement {
+private:
+    Expression* expr1_;
+    Expression* expr2_;
+    char op_;
+    int targetLine_;
+
+public:
+    IfStatement(const std::string& source, Expression* e1, char op, Expression* e2, int line);
+    ~IfStatement();
+    void execute(VarState& state, Program& program) const override;
+};
+
+// END
+class EndStatement : public Statement {
+public:
+    EndStatement(const std::string& source);
+    void execute(VarState& state, Program& program) const override;
+};
+
+// REM This is a comment
+class RemStatement : public Statement {
+public:
+    RemStatement(const std::string& source);
+    void execute(VarState& state, Program& program) const override;
+};
--- src/Statement.cpp
+++ src/Statement.cpp
@@ -14,3 +14,105 @@ Statement::Statement(std::string source) : source_(std::move(source)) {}
 const std::string& Statement::text() const noexcept { return source_; }
 
 // TODO: Imply interfaces declared in the Statement.hpp.
+// LetStatement
+
+LetStatement::LetStatement(const std::string& source, const std::string& var, Expression* expr)
+    : Statement(source), variable_(var), expression_(expr) {}
+
+LetStatement::~LetStatement() {
+    delete expression_;
+}
+
+void LetStatement::execute(VarState& state, Program& program) const {
+    int value = expression_->evaluate(state);
+    state.setValue(variable_, value);  // name-value
+    program.changePC(program.getPC() + 1);
+}
+
+
+
+PrintStatement::PrintStatement(const std::string& source, Expression* expr)
+    : Statement(source), expression_(expr) {}
+
+PrintStatement::~PrintStatement() {
+    delete expression_;
+}
+
+void PrintStatement::execute(VarState& state, Program& program) const {
+    int value = expression_->evaluate(state);
+    std::cout << value << '\n';
+    program.changePC(program.getPC() + 1);
+}
+
+InputStatement::InputStatement(const std::string& source, const std::string& var)
+    : Statement(source), variable_(var) {}
+
+void InputStatement::execute(VarState& state, Program& program) const {
+    int value;
+    std::cout << "?";
+    std::cin >> value;
+
+    if (std::cin.fail()) {
+      std::cin.clear();
+      std::cin.ignore(10000, '\n');
+      throw BasicError("INVALID NUMBER");
+    }
+
+    state.setValue(variable_, value);
+    program.changePC(program.getPC() + 1);
+}
+
+GotoStatement::GotoStatement(const std::string& source, int line)
+    : Statement(source), targetLine_(line) {}
+
+void GotoStatement::execute(VarState& state, Program& program) const {
+    program.changePC(targetLine_);
+}
+
+IfStatement::IfStatement(const std::string& source, Expression* e1, char op, Expression* e2, int line)
+    : Statement(source), expr1_(e1), expr2_(e2), op_(op), targetLine_(line) {}
+
+IfStatement::~IfStatement() {
+    delete expr1_;
+    delete expr2_;
+}
+
+void IfStatement::execute(VarState& state, Program& program) const {
+    int left = expr1_->evaluate(state);
+    int right = expr2_->evaluate(state);
+
+    bool condition = false;
+
+    switch (op_) {
+     case '=':
+        condition = (left == right);
+        break;
+     case '<':
+        condition = (left < right);
+        break;
+     case '>':
+        condition = (left > right);
+        break;
+        default:
+        throw BasicError("SYNTAX ERROR");
+   }
+
+    if (condition) {
+      program.changePC(targetLine_);
+    }
+    else {
+      program.changePC(program.getPC() + 1);  // CHECK:if only line number exists, remove it
+    }
+}
+
+EndStatement::EndStatement(const std::string& source) : Statement(source) {}
+
+void EndStatement::execute(VarState& state, Program& program) const {
+  program.programEnd();
+}
+
+RemStatement::RemStatement(const std::string& source) : Statement(source) {}
+
+void RemStatement::execute(VarState& state, Program& program) const {
+  program.changePC(program.getPC() + 1);
+}
